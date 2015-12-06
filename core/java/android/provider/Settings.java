76409acb270a000b7cae145503e224042213815f
RolanDroid <rolandosimoncelli8@gmail.com>
2015-12-06 11:07:31 +0100

    Up..
    Signed-off-by: RolanDroid <rolandosimoncelli8@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8041,6 +8041,13 @@ public final class Settings {
         public static final String LTE_SERVICE_FORCED = "lte_service_forced";
 
         /**
+         * Toast icon
+         *
+         * @hide
+         */
+        public static final String TOAST_ICON = "toast_icon";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
