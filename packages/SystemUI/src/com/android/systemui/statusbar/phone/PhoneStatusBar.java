0a7c66b92301d87fee8cd1bc5804eb4397c7934c
d34d <clark@cyngn.com>
2015-12-21 11:15:31 -0800

    Keyguard: Pass keyguard events to KeyguardExternalView
    Change-Id: I97c71d28fe125a6474c6d0c5d2484ecec56d309e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -4371,6 +4371,10 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
             mDraggedDownRow = null;
         }
         mAssistManager.onLockscreenShown();
+        if (mNotificationPanel.hasExternalKeyguardView()) {
+            mNotificationPanel.getExternalKeyguardView().onKeyguardShowing(
+                    mStatusBarKeyguardViewManager.isScreenTurnedOn());
+        }
     }
 
     private void onLaunchTransitionFadingEnded() {
@@ -4521,6 +4525,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mNotificationPanel.onAffordanceLaunchEnded();
         mNotificationPanel.animate().cancel();
         mNotificationPanel.setAlpha(1f);
+        if ( mNotificationPanel.getExternalKeyguardView() != null) {
+            mNotificationPanel.getExternalKeyguardView().onKeyguardDismissed();
+        }
         return staying;
     }
 
@@ -4960,6 +4967,15 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         mScreenTurningOn = false;
         mDozeScrimController.onScreenTurnedOn();
         mVisualizerView.setVisible(true);
+        if (mNotificationPanel.hasExternalKeyguardView()) {
+            mNotificationPanel.getExternalKeyguardView().onScreenTurnedOn();
+        }
+    }
+
+    public void onScreenTurnedOff() {
+        if (mNotificationPanel.hasExternalKeyguardView()) {
+            mNotificationPanel.getExternalKeyguardView().onScreenTurnedOff();
+        }
     }
 
     /**
