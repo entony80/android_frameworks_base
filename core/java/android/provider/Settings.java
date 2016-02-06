8233e23b5e3689ffa816b3ddf3c06feb96535d92
LorDClockaN <davor@losinj.com>
2016-02-06 20:46:49 +0100

    FWB: Status bar header font style (1/2)
    Change-Id: I79d18c889858919b13aa98c3dc8e56a0145f9548

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4733,6 +4733,12 @@ public final class Settings {
         public static final String MEDIA_SCANNER_ON_BOOT = "media_scanner_on_boot";
 
         /**
+         * Custom status bar header font style
+         * @hide
+         */
+        public static final String STATUS_BAR_HEADER_FONT_STYLE = "status_bar_header_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
